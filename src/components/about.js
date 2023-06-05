858d064cb385167161bc3ea7bc1e478ff9c138f0
romaric.guth <romaric.guth@epita.fr>
2023-06-05 17:50:23 +0200

    add explanations about "why you need me" section

--- src/components/about.js
+++ src/components/about.js
@@ -5,15 +5,31 @@ import { useTranslation } from "../utils/translation/client_utils"
 
 export default function About() {
     const { t } = useTranslation('home');
+    const needs = [
+        {
+            text: t('need1'),
+            why: t('need1Explanation')
+        },
+        {
+            text: t('need2'),
+            why: t('need2Explanation')
+        },
+    ]
 
     return (
         <div>
             <h3 class="mb-8">{t('needMe')}</h3>
-            <ul className="list-disc list-inside list">
-                <li>{t('need1')}</li>
-                <li>{t('need2')}</li>
-                <li>{t('need3')}</li>
-            </ul>
+            <dl className="list-disc list-inside list gap-4">
+                {needs.map((need, index) => (
+                    <div key={index} class="mt-4">
+                        <dt class="list-item">{need.text}</dt>
+                        <dd class="ml-8 mt-2 flex gap-4 text-justify">
+                            <i class="fa-solid fa-arrow-right-long"></i>
+                            {need.why}
+                        </dd>
+                    </div>
+                ))}
+            </dl>
             <div class="flex flex-row items-center gap-2 mt-16 mb-8"> 
                 <h3>{t('storyTime')}</h3>
                 <Image src="/assets/pen.png" alt="picture of a pen" width={30} height={30} />
