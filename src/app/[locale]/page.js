c6b75c255511a68a5c50e900a52e361745c7953a
romaric.guth <ro.guth@free.fr>
2025-03-31 13:02:54 +0200

    add canonical and alternates meta tags

--- src/app/[locale]/page.js
+++ src/app/[locale]/page.js
@@ -1,7 +1,15 @@
-import Home from "../../components/home"
+import Home from "../../components/home";
+
+export const metadata = {
+  alternates: {
+    canonical: "https://romaricguth.com",
+    languages: {
+      en: "https://romaricguth.com/en",
+      fr: "https://romaricguth.com/fr",
+    },
+  },
+};
 
 export default async function HomePage() {
-  return (
-    <Home />
-  )
+  return <Home />;
 }
